567ae168c178e473714396bdce8eb451328f079f
LuckilyChensy <LuckilyChensy@163.com>
2021-09-12 23:10:20 +0800

    栈结构实现

---
+++ algorithms-sedgewick/src/main/java/com/soso/chapter01/ArrayStack.java
@@ -0,0 +1,34 @@
+package com.soso.chapter01;
+
+/**
+ * @Description:
+ * @Date:Created in 23:09 2021/9/12
+ */
+public class ArrayStack<E> implements Stack {
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void push(Object o) {
+
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+}
