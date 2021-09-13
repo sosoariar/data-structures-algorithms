ddda8e9461244c6bf53a5aa09427057c0fc1199f
LuckilyChensy <LuckilyChensy@163.com>
2021-09-14 07:04:33 +0800

    BST contains

--- algorithms-sedgewick/src/main/java/com/soso/chapter01/BST.java
+++ algorithms-sedgewick/src/main/java/com/soso/chapter01/BST.java
@@ -32,4 +32,39 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty(){
         return size == 0;
     }
+
+    // 向以node为根的二分搜索树中插入元素e，递归算法
+    // 返回插入新节点后二分搜索树的根
+    private Node add(Node node, E e){
+        if(node == null){
+            size ++;
+            return new Node(e);
+        }
+
+        if(e.compareTo(node.e) < 0)
+            node.left = add(node.left, e);
+        else if(e.compareTo(node.e) > 0)
+            node.right = add(node.right, e);
+
+        return node;
+    }
+
+    // 看二分搜索树中是否包含元素e
+    public boolean contains(E e){
+        return contains(root, e);
+    }
+
+    // 看以node为根的二分搜索树中是否包含元素e, 递归算法
+    private boolean contains(Node node, E e){
+
+        if(node == null)
+            return false;
+
+        if(e.compareTo(node.e) == 0)
+            return true;
+        else if(e.compareTo(node.e) < 0)
+            return contains(node.left, e);
+        else // e.compareTo(node.e) > 0
+            return contains(node.right, e);
+    }
 }
