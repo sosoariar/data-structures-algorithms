48b99f44418a5b099d7671bc6fda2dd3618e4400
LuckilyChensy <LuckilyChensy@163.com>
2021-09-13 19:58:42 +0800

    ArrarStack

--- algorithms-sedgewick/src/main/java/com/soso/chapter01/ArrayStack.java
+++ algorithms-sedgewick/src/main/java/com/soso/chapter01/ArrayStack.java
@@ -4,31 +4,59 @@ package com.soso.chapter01;
  * @Description:
  * @Date:Created in 23:09 2021/9/12
  */
-public class ArrayStack<E> implements Stack {
+public class ArrayStack<E> implements Stack<E> {
+
+    private ArrayList<E> arrayList;
+
+    public ArrayStack(int capacity){
+        arrayList = new ArrayList<>(capacity);
+    }
+
+    public ArrayStack(){
+        arrayList = new ArrayList<>();
+    }
 
     @Override
     public int getSize() {
-        return 0;
+        return arrayList.getSize();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return arrayList.isEmpty();
+    }
+
+    public int getCapacity(){
+        return arrayList.getCapacity();
     }
 
     @Override
-    public void push(Object o) {
-
+    public void push(E e) {
+        arrayList.addLast(e);
     }
 
     @Override
-    public Object pop() {
-        return null;
+    public E pop() {
+        return arrayList.removeLast();
     }
 
     @Override
-    public Object peek() {
-        return null;
+    public E peek() {
+        return arrayList.getLast();
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append("Stack: ");
+        res.append('[');
+        for(int i = 0 ; i < arrayList.getSize() ; i ++){
+            res.append(arrayList.get(i));
+            if(i != arrayList.getSize() - 1)
+                res.append(", ");
+        }
+        res.append("] top");
+        return res.toString();
     }
 
 }
