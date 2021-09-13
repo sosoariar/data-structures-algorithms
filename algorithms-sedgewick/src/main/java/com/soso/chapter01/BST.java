90af4330d88070e558af7c93fe19b75f8853dc9f
LuckilyChensy <LuckilyChensy@163.com>
2021-09-14 07:15:26 +0800

    BST preorder

--- algorithms-sedgewick/src/main/java/com/soso/chapter01/BST.java
+++ algorithms-sedgewick/src/main/java/com/soso/chapter01/BST.java
@@ -68,6 +68,46 @@ public class BST<E extends Comparable<E>> {
             return contains(node.right, e);
     }
 
+    // 二分搜索树的前序遍历
+    public void preOrder(){
+        preOrder(root);
+    }
 
+    // 前序遍历以node为根的二分搜索树, 递归算法
+    private void preOrder(Node node){
+
+        if(node == null)
+            return;
+
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    // 生成以node为根节点，深度为depth的描述二叉树的字符串
+    private void generateBSTString(Node node, int depth, StringBuilder res){
+
+        if(node == null){
+            res.append(generateDepthString(depth) + "null\n");
+            return;
+        }
+
+        res.append(generateDepthString(depth) + node.e + "\n");
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+    }
+
+    private String generateDepthString(int depth){
+        StringBuilder res = new StringBuilder();
+        for(int i = 0 ; i < depth ; i ++)
+            res.append("--");
+        return res.toString();
+    }
 
 }
